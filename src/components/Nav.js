d8419be2b28f67ed443c2cc783d5fad488404c8a
Hossain <arafat.kd99@gmail.com>
2023-09-02 15:59:28 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import Books from './Books';
+import { Link } from 'react-router-dom';
 // import userIcon from '../Assets/user-regular.svg';
 
 const Nav = () => (
   <nav>
     <h1>Bookstore CMS</h1>
-    <ul>
-      <li to="/">Books</li>
-      <li to="/categories">Categories</li>
+    <ul className={Nav.ul}>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/calculator">Calculator</Link></li>
+      <li><Link to="/quote">Quote</Link></li>
     </ul>
     {/* <a to="/"><userIcon /></a> */}
   </nav>
