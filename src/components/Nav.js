651e76081427a0045f0ee8757ae8339345bf6ba3
Hossain <arafat.kd99@gmail.com>
2023-09-02 16:06:45 +0100

    Edit text to align with template design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,9 +6,8 @@ const Nav = () => (
   <nav>
     <h1>Bookstore CMS</h1>
     <ul className={Nav.ul}>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/calculator">Calculator</Link></li>
-      <li><Link to="/quote">Quote</Link></li>
+      <li><Link to="/">BOOKS</Link></li>
+      <li><Link to="/categories">CATEGORIES</Link></li>
     </ul>
     {/* <a to="/"><userIcon /></a> */}
   </nav>
