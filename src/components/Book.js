40b7eba810eb3edec58871bb0a3a8099c62f356f
Hossain <arafat.kd99@gmail.com>
2023-09-02 14:28:26 +0100

    Valifate propsType

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,28 @@
 // Individual Book component
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = ({ title, author, category }) => (
-    <div className="book">
-      <h3>{title}</h3>
-      <p>Author: {author}</p>
-      <p>Category: {category}</p>
-    </div>
+  <div className="book">
+    <h3>
+      Title:
+      {title}
+    </h3>
+    <p>
+      Author:
+      {author}
+    </p>
+    <p>
+      Category:
+      {category}
+    </p>
+  </div>
 );
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
 
 export default Book;
