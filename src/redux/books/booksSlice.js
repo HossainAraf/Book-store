9c456a48cdba3e363bfdbe4118c13a66a7b16593
Hossain <arafat.kd99@gmail.com>
2023-09-03 17:12:24 +0100

    Add reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,22 +1,43 @@
 // DEFINING CONSTANTS
-const Add_Book = "Add_Book";
-const Remove_Book = "Remove_Book";
+const AddBook = 'AddBook';
+const RemoveBook = 'RemoveBook';
 
+// const { type } = require("@testing-library/user-event/dist/type");
 
-
-const { type } = require("@testing-library/user-event/dist/type");
-
+// STATE
 const initialBooksState = {
   books: [],
 };
 
-// ACTION
-// add a book
-// remove a book
-const removeBookAction = () =>  {
-      type: Remove_Book,
-};
+//  ACTION CREATORS
+// add a book || remove a book
 
-const addBookAction = () =>  {
-      type: Add_Book,
+const addBookAction = (title) => ({
+  type: AddBook,
+  payload: { title },
+});
+
+const removeBookAction = (title) => ({
+  type: RemoveBook,
+  payload: { title },
+});
+
+// REDUCER
+const addBookReducer = (state = initialBooksState, action) => {
+  switch (action.type) {
+    case AddBook:
+      return {
+        ...state,
+        books: [...state.books, action.payload.title],
+      };
+
+    case RemoveBook:
+      return {
+        ...state,
+        books: state.books.filter((book) => book !== action.payload.title),
+      };
+
+    default:
+      return state;
+  }
 };
