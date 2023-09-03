31c02269856f449cd765f5508a7a91f978784758
Hossain <arafat.kd99@gmail.com>
2023-09-03 15:24:46 +0100

    Set action for Add & Remove

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,4 +1,22 @@
+// DEFINING CONSTANTS
+const Add_Book = "Add_Book";
+const Remove_Book = "Remove_Book";
+
+
+
+const { type } = require("@testing-library/user-event/dist/type");
 
 const initialBooksState = {
   books: [],
-}
+};
+
+// ACTION
+// add a book
+// remove a book
+const removeBookAction = () =>  {
+      type: Remove_Book,
+};
+
+const addBookAction = () =>  {
+      type: Add_Book,
+};
