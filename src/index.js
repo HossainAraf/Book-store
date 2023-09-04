65b1f4552cc38a68fcfa3d9a132d7c7ac99d7dfc
Hossain <arafat.kd99@gmail.com>
2023-09-04 15:53:33 +0100

    Add & Import categoriesReducer to store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+// import { store } from './redux/store';
+// import { provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 );
 
 reportWebVitals();
