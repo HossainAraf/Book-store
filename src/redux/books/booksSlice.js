6dfdab77966b002322deb64da8c5e34e11783d8a
Hossain <arafat.kd99@gmail.com>
2023-09-04 10:19:49 +0100

    Implement createSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,38 +1,27 @@
-import { createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 // DEFINING CONSTANTS
 const AddBook = 'AddBook';
 const RemoveBook = 'RemoveBook';
-
-// const { type } = require("@testing-library/user-event/dist/type");
 
 // STATE
 const initialBooksState = {
   books: [],
 };
 
-//  ACTION CREATORS
-// add a book || remove a book
-
-const addBookAction = (title) => ({
-  type: AddBook,
-  payload: { title },
-});
-
-const removeBookAction = (title) => ({
-  type: RemoveBook,
-  payload: { title },
-});
-
-// REDUCER
-const bookReducer = createReducer(initialBooksState, {
-  [AddBook]: (state, action) => {
+//CREATE A SLICE USING createSlice
+const bookSlice = createSlice({
+  name: 'books',
+  initialState: initialBooksState,
+  reducers: {
+  addBook:(state, action) => {
     state.books.push(action.payload.title);
   },
-
-  [RemoveBook]: (state, action) => {
-    state.books = state.books.filter((book) => book !== action.payload.title);
-  },
+  removeBook: (state, action) => {
+    state.books = state.books.filer((book) => book !== action.payload
+},
+},
 });
+
 
 export default bookReducer;
