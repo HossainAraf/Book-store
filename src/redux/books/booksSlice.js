9b556519683d83e6c685d31364f98931092f7e57
Hossain <arafat.kd99@gmail.com>
2023-09-08 21:05:06 +0100

    Update async actions

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,13 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const APIurl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/SQNY5l4rmszadPvg3zaz/books'; // 6QJZ6Z2X2X
+// API URL
+const API_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/SQNY5l4rmszadPvg3zaz/books';
 
-// CREATE ASYNC THUNK
-export const fetchBooks = createAsyncThunk(
-  'books/fetchBooks',
-  async () => {
-    const response = await axios.get(APIurl);
-    return response.data;
-  },
-);
+// ASYNC ACTIONS
+const fetchBooks = createAsyncThunk('DisplayBooks', async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+});
+
+const addBook = createAsyncThunk('AddBook', async (book) => {
+  const response = await axios.post(API_URL, book);
+  return response.data === 'Created' ? book : null;
+});
+
+const removeBook = createAsyncThunk('RemoveBook', async (ITEM_ID) => {
+  const response = await axios.delete(`${API_URL}/${ITEM_ID}`);
+  return response.data === 'The book is removed!' ? ITEM_ID : null;
+});
