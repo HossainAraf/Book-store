518e3787846ae875cb7fc3f7928194136f0873a6
Hossain <arafat.kd99@gmail.com>
2023-09-14 15:18:04 +0100

    Add comments at Categories.js module

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,9 @@
+// IMPORTS
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateStatus } from '../redux/categories/categoriesSlice';
 
+// CREATE A COMPONENT
 const Categories = () => {
   const dispatch = useDispatch();
   const categoriesStatus = useSelector((state) => state.categories.status);
@@ -10,6 +12,7 @@ const Categories = () => {
     dispatch(updateStatus());
   };
 
+  // RETURN JSX
   return (
     <>
       <h1>Categories</h1>
