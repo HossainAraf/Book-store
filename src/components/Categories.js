3f2dd96c66467d8b44974e5290f074849bd5ad7b
Hossain <arafat.kd99@gmail.com>
2023-09-14 15:05:32 +0100

    Update categories component to update status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateStatus } from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <>
-    <h1>Categories</h1>
-    <button type="button">Status</button>
-  </>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categoriesStatus = useSelector((state) => state.categories.status);
+
+  const handleUpdateStatus = () => {
+    dispatch(updateStatus());
+  };
+
+  return (
+    <>
+      <h1>Categories</h1>
+      <p>
+        Status:
+        {categoriesStatus}
+      </p>
+      <button type="button" onClick={handleUpdateStatus}>
+        Update Status
+      </button>
+    </>
+  );
+};
 
 export default Categories;
